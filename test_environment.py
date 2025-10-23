a445d845367dcb73b6808265602f1e7a6a2443b3
tabyldyyevam1 <madina.tayldy@icloud.com>
2025-10-23 13:11:42 +0000

    Initial commit

---
+++ test_environment.py
@@ -0,0 +1,8 @@
+import fastapi, psycopg2, redis, requests
+print("✅ All main Python packages are successfully installed!")
+
+import os
+os.system("docker --version")
+os.system("nginx -v")
+os.system("psql --version")
+
